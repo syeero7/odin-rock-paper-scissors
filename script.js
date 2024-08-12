b6fed47bab853d913bf8d85a5ca98acc595baa77
SyeerO7 <EmalHasitha@proton.me>
2024-08-12 10:29:09 +0530

    Add a function to play a single round

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
 let humanScore = 0;
 let computerScore = 0;
 
@@ -22,3 +25,31 @@ function getHumanChoice() {
     );
   }
 }
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === "rock" && computerChoice === "paper") {
+    console.log("You lose! Paper beats Rock");
+    computerScore += 1;
+  } else if (humanChoice === "scissors" && computerChoice === "rock") {
+    console.log("You lose! Rock beats Scissors");
+    computerScore += 1;
+  } else if (humanChoice === "paper" && computerChoice === "scissors") {
+    console.log("You lose! Scissors beats Paper");
+    computerScore += 1;
+  } else if (humanChoice === "paper" && computerChoice === "rock") {
+    console.log("You win! Paper beats Rock");
+    humanScore += 1;
+  } else if (humanChoice === "rock" && computerChoice === "scissors") {
+    console.log("You win! Rock beats Scissors");
+    humanScore += 1;
+  } else if (humanChoice === "scissors" && computerChoice === "paper") {
+    console.log("You win! Scissors beats Paper");
+    humanScore += 1;
+  } else if (humanChoice === computerChoice) {
+    console.log(" It's a tie!");
+  } else {
+    console.log("Your choice is invalid. Please try again");
+  }
+}
+
+playRound(humanSelection, computerSelection);
